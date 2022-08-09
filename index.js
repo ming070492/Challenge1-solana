29f7e8965ffb6b9df55a9265e164b2449fdcbc00
ming070492 <93239977+ming070492@users.noreply.github.com>
2022-08-09 20:22:44 +0800

    Update index.js

--- index.js
+++ index.js
@@ -7,18 +7,12 @@ const {
     LAMPORTS_PER_SOL
 } = require("@solana/web3.js");
 
+const userWalletPubKey = process.argv.slice(2);
+
 // Create a new keypair
-const newPair = new Keypair();
-
-// Exact the public and private key from the keypair
-
-const publicKey = new PublicKey(newPair._keypair.publicKey).toString();
-const privateKey = newPair._keypair.secretKey;
 
 // Connect to the Devnet
 const connection = new Connection(clusterApiUrl("devnet"), "confirmed");
-
-console.log("Public Key of the generated keypair", publicKey);
 
 // Get the wallet balance from a given private key
 const getWalletBalance = async () => {
@@ -28,9 +22,8 @@ const getWalletBalance = async () => {
         //console.log("Connection object is:", connection);
 
         // Make a wallet (keypair) from privateKey and get its balance
-        const myWallet = await Keypair.fromSecretKey(privateKey);
         const walletBalance = await connection.getBalance(
-            new PublicKey(newPair.publicKey)
+            new PublicKey(userWalletPubKey[0])
         );
         console.log(`Wallet balance: ${parseInt(walletBalance) / LAMPORTS_PER_SOL} SOL`);
     } catch (err) {
@@ -42,12 +35,11 @@ const airDropSol = async () => {
     try {
         // Connect to the Devnet and make a wallet from privateKey
         const connection = new Connection(clusterApiUrl("devnet"), "confirmed");
-        const myWallet = await Keypair.fromSecretKey(privateKey);
 
         // Request airdrop of 2 SOL to the wallet
-        console.log("Airdropping some SOL to my wallet!");
+        console.log("Airdropping to user\' account...");
         const fromAirDropSignature = await connection.requestAirdrop(
-            new PublicKey(myWallet.publicKey),
+            new PublicKey(userWalletPubKey[0]),
             2 * LAMPORTS_PER_SOL
         );
         await connection.confirmTransaction(fromAirDropSignature);
@@ -64,3 +56,5 @@ const mainFunction = async () => {
 }
 
 mainFunction();
+
+
